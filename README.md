766d31f934ba773fb025e5717e35eed92cd63aae
James Johnson <johnsojr@gmail.com>
2013-11-09 14:27:43 -0600

    initial commit again

--- README.md
+++
@@ -1,6 +0,0 @@
-rebuild-bkwld.com
-=================
-
-The new [BKWLD](http://bkwld.com) is hot. 
-
-This is my attempt to reengineer their site and learn something about web development in the process.
---
+++ proto/js/main.js
@@ -0,0 +1,33 @@
+;(function($, window, document, undefined) {
+  "use strict";
+  var modulesPage = {
+    settings: {
+      modulesPageSelector: "body.modules-page",
+      mockupSelector: ".mockup img"
+    },
+
+    init: function() {
+      this.retinaImages();
+    },
+
+    retinaImages: function() {
+      var mockupSel = this.settings.modulesPageSelector + " " + this.settings.mockupSelector;
+      var $mockups = $(mockupSel);
+      // Add a 50% width attribute to scale down screenshots to 50%.
+      // This assumes all mockup images are retina
+      $mockups.each(function() {
+        var w = $(this).css('width');
+        var newWidth = parseInt(w,10)/2;
+       $(this).css('border','1px solid red')
+        .css('width', newWidth + 'px');
+
+      });
+    }
+
+  };
+
+  $(document).ready(function() {
+    modulesPage.init();
+  });
+
+})(jQuery, window, document);
